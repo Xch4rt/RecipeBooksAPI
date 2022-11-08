657815d50e3362a3bc1ba618f42bfacfd4e00ec7
Xch4rt <pab203.guti@gmail.com>
2022-11-07 22:48:21 -0600

    CRUD Ingredients

--- src/recipebook/ingredients/dto/create-ingredient.dto.ts
+++ src/recipebook/ingredients/dto/create-ingredient.dto.ts
@@ -1,1 +1,7 @@
-export class CreateIngredientDto {}
+import { IsNumber, IsString } from 'class-validator';
+export class CreateIngredientDto {
+  @IsNumber()
+  recipe_id: number;
+  @IsString()
+  instrucction: string; // me confundi XD aqui deberia de llamarse ingredient_name
+}
--- src/recipebook/ingredients/ingredients.service.ts
+++ src/recipebook/ingredients/ingredients.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateIngredientDto } from './dto/create-ingredient.dto';
 import { UpdateIngredientDto } from './dto/update-ingredient.dto';
 
 @Injectable()
 export class IngredientsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createIngredientDto: CreateIngredientDto) {
-    return 'This action adds a new ingredient';
+    return this.prisma.ingredients.create({
+      data: {
+        recipe_id: createIngredientDto.recipe_id,
+        instrucction: createIngredientDto.instrucction,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all ingredients`;
+    return this.prisma.ingredients.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} ingredient`;
+    return this.prisma.ingredients.findUnique({
+      where: {
+        id: id,
+      },
+    });
   }
 
   update(id: number, updateIngredientDto: UpdateIngredientDto) {
-    return `This action updates a #${id} ingredient`;
+    return this.prisma.ingredients.update({
+      where: {
+        id: id,
+      },
+      data: {
+        recipe_id: updateIngredientDto.recipe_id,
+        instrucction: updateIngredientDto.instrucction,
+      },
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} ingredient`;
+    return this.prisma.ingredients.update({
+      where: {
+        id: id,
+      },
+      data: {
+        isAvailable: false,
+      },
+    });
   }
 }
